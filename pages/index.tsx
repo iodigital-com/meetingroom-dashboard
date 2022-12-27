a535292713f2444b38e1041befb015dc25b2abb0
Tameem <tameem.zaaiter@frontmen.nl>
2022-12-27 14:11:57 +0100

    add unavailable room colors to the svg

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,7 @@ import Amsterdam1Map from '../map-components/amsterdam1'
 
 export default function Home() {
   return (
-    <div>
-      {/* uncomment to show Amsterdam's map */}
+    <div style={{ marginLeft: 'auto', marginRight: 'auto', marginTop: 120}}>
       <Amsterdam1Map />
     </div>
   )
