607302956569d3912de3f3520674cca802a3a87f
Your Name <you@example.com>
2023-04-25 11:37:26 +0200

    bug fixes

--- frontend/__tests__/index.test.tsx
+++ frontend/__tests__/index.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 // import userEvent from '@testing-library/user-event';
-import Home from '../index';
+import Home from '../pages/index';
 
 describe('Home', () => {
   it('renders the heading', () => {
     render(<Home />);
-    const heading = screen.queryByText('Welcome to iO meeting room app where you can see available meeting rooms in real time');
+    const heading = screen.queryByText(
+      'Welcome to iO meeting room app where you can see available meeting rooms in real time'
+    );
     expect(heading).toBeInTheDocument();
   });
 
